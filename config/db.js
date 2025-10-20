1684e0db175151da4e9403ff269f4f949f1eb784
Riya Verma <rv9858196@gmail.com>
2025-10-21 03:08:00 +0530

    Add MySQL pool with environment variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,25 @@
-const mysql = require('mysql2/promise')
+// const mysql = require('mysql2/promise')
+
+// const mySqlPool = mysql.createPool({
+//     host: "localhost" ,
+//     user: "root" ,
+//     password: "Riyaverma@mysql" ,
+//     database: "schools_db" ,
+// })
+
+// module.exports = mySqlPool ;.
+
+
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const mySqlPool = mysql.createPool({
-    host: "localhost" ,
-    user: "root" ,
-    password: "Riyaverma@mysql" ,
-    database: "schools_db" ,
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
-module.exports = mySqlPool ;
+module.exports = mySqlPool;
